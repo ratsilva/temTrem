374eaa580e5fd6bd6deeb3880f435f528a74f167
Ricardo Silva <ricardoatsilva1@gmail.com>
2019-11-10 15:57:31 -0300

    Fixing index.js to remove commmand line parameters

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-if (process.env.TOKEN || process.env.SLACK_TOKEN) {
-    //Treat this as a custom integration
-    var customIntegration = require('./lib/custom_integrations');
-    var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
-    var controller = customIntegration.configure(token, config, onInstallation);
-} else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
-    //Treat this as an app
-    var app = require('./lib/apps');
-    var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
-} else {
-    console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
-    process.exit(1);
+process.env.CLIENT_ID = '830684567270.820849481649'
+process.env.CLIENT_SECRET = '44aa89946a83649b6d764ec6b4a05522'
+process.env.PORT = '8765'
+
+function onInstallation(bot, installer) {
+    if (installer) {
+        bot.startPrivateConversation({user: installer}, function (err, convo) {
+            if (err) {
+                console.log(err);
+            } else {
+                convo.say('I am a bot that has just joined your team');
+                convo.say('You must now /invite me to a channel so that I can be of use!');
+            }
+        });
+    }
 }
 
+var app = require('./lib/apps');
+    var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, onInstallation);
+
 controller.on('bot_channel_join', function (bot, message) {
-    bot.reply(message, "I'm here!")
+    bot.reply(message, "Olá! Você pode me enviar `hello` ou `status`")
 });
 
 controller.hears('hello', 'direct_message,direct_mention,mention', function (bot, message) {
-    bot.reply(message, 'Hello!');
+    bot.reply(message, 'Hello Human!');
 });
 
-controller.hears('/status', 'direct_message,direct_mention,mention', function (bot, message) {
-    bot.reply(message, 'Atualmente estamos no trem #3');
+controller.hears('status', 'direct_message,direct_mention,mention', function (bot, message) {
+    bot.reply(message, 'Estamos atualmente no `trem #3` de Android!');
 });
